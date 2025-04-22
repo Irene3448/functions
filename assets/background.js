7f9b93a0882e3558b8f9d948dc03077d66f152d1
Jae Hyun Ahn <ahnj233@newschool.edu>
2025-04-21 22:20:08 -0400

    open and hide the intro layer

--- assets/background.js
+++ assets/background.js
@@ -16,4 +16,20 @@ function updateBackground(mood) {
 	  const selectedImage = moodImages[mood] || moodImages.default;
 	  bgImage.src = `assets/images/${selectedImage}`;
 	}
-  }
+}
+
+//Intro (#about-app) page display
+window.addEventListener("DOMContentLoaded", ()=>{
+	const aboutPage = document.getElementById("about-page");
+	const startButton = document.getElementById("start-app");
+
+	//show about page on first load
+	aboutPage.style.display="block";
+	startButton.style.display="block";
+
+	//hide about page on button click
+	startButton.addEventListener("click", ()=>{
+		aboutPage.style.display="none";
+		startButton.style.display="none";
+	});
+});
